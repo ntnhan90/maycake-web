8ab325a39b293e0f5f2b3b17ea0fbbdce08540d4
nguyễn thái nhân <ntnhan90@gmail.com>
2025-11-12 10:06:27 +0700

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,23 +2,23 @@ import NextAuth, { User } from 'next-auth'
 import { authOptions } from '@/app/api/auth/option'
 
 declare module 'next-auth' {
-  interface User {
-    id: number;
-    username: string;
-    name: string;
-    email: string;
-    avatar: string;
-  }
+    interface User {
+      id: number;
+      username: string;
+      name: string;
+      email: string;
+      avatar: string;
+    }
 
-  interface Session {
-    user: User;
-  }
+    interface Session {
+      user: User;
+    }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT {
-    user: User;
-  }
+    interface JWT {
+        user: User;
+    }
 }
 
 const handler = NextAuth(authOptions)
--- src/app/api/auth/option.ts
+++ src/app/api/auth/option.ts
@@ -3,47 +3,47 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { getDictionary } from '@/locales/dictionary'
 
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    async jwt({ user, token }) {
-      if (user) {
-        return { ...token, user: { ...user as User } }
-      }
+    callbacks: {
+        async jwt({ user, token }) {
+            if (user) {
+                return { ...token, user: { ...user as User } }
+            }
 
-      return token
+            return token
+        },
+        async session({ session, token }) {
+            return { ...session, user: token.user }
+        },
     },
-    async session({ session, token }) {
-      return { ...session, user: token.user }
-    },
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        username: { type: 'string' },
-        password: { type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials) {
-          return null
-        }
-        const { username, password } = credentials
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                username: { type: 'string' },
+                password: { type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials) {
+                return null
+                }
+                const { username, password } = credentials
 
-        // Replace with real authentication here
-        const ok = username === 'Username' && password === 'Password'
+                // Replace with real authentication here
+                const ok = username === 'Username' && password === 'Password'
 
-        const dict = await getDictionary()
+                const dict = await getDictionary()
 
-        if (!ok) {
-          throw new Error(dict.login.message.auth_failed)
-        }
+                if (!ok) {
+                throw new Error(dict.login.message.auth_failed)
+                }
 
-        return {
-          id: 1,
-          name: 'Name',
-          username: 'Username',
-          email: 'user@email.com',
-          avatar: '/assets/img/avatars/8.jpg',
-        }
-      },
-    }),
-  ],
+                return {
+                id: 1,
+                name: 'Name',
+                username: 'Username',
+                email: 'user@email.com',
+                avatar: '/assets/img/avatars/8.jpg',
+                }
+            },
+        }),
+    ],
 }
