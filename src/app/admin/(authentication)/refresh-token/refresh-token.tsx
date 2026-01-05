fa79d493d833bbade3ef9f98c69000ec4d074c38
nguyễn thái nhân <ntnhan90@gmail.com>
2026-01-05 22:28:29 +0700

    refresh tokem

---
+++ src/app/admin/(authentication)/refresh-token/refresh-token.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function RefreshToken() {
+    return(
+       <div>Refresh token....</div>
+    )
+}
